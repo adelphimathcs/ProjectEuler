8282e282d60390d0e64fc57f0f66bd1e2a230466
matt matero <matthewmatero@mail.adelphi.edu>
2016-10-05 14:11:57 -0400

    Fixed ML solution for PE0048

--- 0048/ML/PE0048-MM.ml
+++ 0048/ML/PE0048-MM.ml
@@ -1,1 +1,6 @@
-foldl (op +) 0 (List.tabulate(1000, fn x => (x+1) * (x+1)));
+open IntInf;
+
+fun power (x,0) = 1 |
+	power (x,n) = x * power(x, n-1);
+
+foldl (op +) 0 (List.tabulate(1000, fn x => power((fromInt(x)+1),(fromInt(x)+1))));
